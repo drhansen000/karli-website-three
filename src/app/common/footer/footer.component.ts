1f782d8a2eed587e6c4e4968cee2d8e84e750031
drhansen000 <drhansen000@hotmail.com>
2020-03-26 15:22:28 -0600

    Created basic skeleton

---
+++ src/app/common/footer/footer.component.ts
@@ -0,0 +1,38 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-footer',
+  templateUrl: './footer.component.html',
+  styleUrls: ['./footer.component.css']
+})
+export class FooterComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+    this.populateLastModified();
+  }
+  populateLastModified() {
+    const days = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
+    const months = [
+      'January',
+      'February',
+      'March',
+      'April',
+      'May',
+      'June',
+      'July',
+      'August',
+      'September',
+      'October',
+      'November',
+      'December'];
+    const modifiedDate = new Date(document.lastModified);
+    let modifiedDateString = days[modifiedDate.getDay()] + ', ';
+    modifiedDateString += months[modifiedDate.getMonth()] + ' ';
+    modifiedDateString += modifiedDate.getDate() + ', ';
+    modifiedDateString += modifiedDate.getFullYear();
+    document.querySelector('#lastModified').textContent = modifiedDateString;
+  }
+
+}
